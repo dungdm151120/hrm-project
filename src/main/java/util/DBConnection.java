8113f3174e750ccd76ddb4216d95189255645a31
Nguyen Sang <sangddk2k6@gmail.com>
2026-05-14 22:52:01 +0700

    thay đổi 1 số

---
+++ src/main/java/util/DBConnection.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/hrm_db?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "S@ng130206";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC Driver not found. Please add mysql-connector-j to the project.", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
--- src/main/webapp/WEB-INF/views/admin/user-detail.jsp
+++ src/main/webapp/WEB-INF/views/admin/user-detail.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: Admin
   Date: 5/14/2026
-  Time: 10:23 PM
+  Time: 10:25 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
