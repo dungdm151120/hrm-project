e5dc680dcd9790b9134140f06a7bdabdaf9b0045
Nguyen Sang <sangddk2k6@gmail.com>
2026-05-15 16:01:15 +0700

    Merge single file from develop

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,133 @@
 package model;
 
+import java.util.Date;
+
 public class User {
+    private int id;
+    private String full_name;
+    private String email;
+    private String password;
+    private String phone;
+    private String gender;
+    private Date date_of_birth;
+    private String address;
+    private String avatar_url;
+    private int role_id;
+    private String role_name;
+    private boolean active;
+    private String reset_token;
+    private Date reset_token_expired_at;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+    public String getRole_name() {
+        return role_name;
+    }
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getReset_token() {
+        return reset_token;
+    }
+
+    public void setReset_token(String reset_token) {
+        this.reset_token = reset_token;
+    }
+
+    public Date getReset_token_expired_at() {
+        return reset_token_expired_at;
+    }
+
+    public void setReset_token_expired_at(Date reset_token_expired_at) {
+        this.reset_token_expired_at = reset_token_expired_at;
+    }
 }
